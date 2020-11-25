473c5f822ba902533ab7194f5ee65ea13c4d3dc1
balkasem <osb276@g.harvard.edu>
2020-11-24 23:36:04 -0500

    changes

---
+++ p2/src/components/ShowPost.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Title: {{ post.title }}</div>
+    <p v-if="includeDetails">{{ post.content }}</p>
+    <div v-if="includeDetails">${{ post.auther }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "show-post",
+  props: ["post", "includeDetails"],
+  data: function() {
+    return {};
+  }
+};
+</script>
