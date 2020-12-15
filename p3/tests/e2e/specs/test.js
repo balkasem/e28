40c2a2026eff291cd236caf861446ff633e19766
balkasem <osb276@g.harvard.edu>
2020-12-15 14:27:00 -0500

    changes for e2e test

---
+++ p3/tests/e2e/specs/test.js
@@ -0,0 +1,8 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('My First Test', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/')
+    cy.contains('p', 'Home Page for my Blog - Articles Written by Osama Balkasem')
+  })
+})
